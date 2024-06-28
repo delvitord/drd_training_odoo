5c10e96a4ec3659c77500b7fb0af9a9f78b343b1
delvitord <vitorahim@gmail.com>
2024-06-28 11:19:05 +0700

    first commit
    Add Models: Passanger, Bus, Bus Route, Bus Schedulue, HR Employee

---
+++ models/bus.py
@@ -0,0 +1,21 @@
+-*- coding: utf-8 -*-
+
+from odoo import models, fields 
+
+class Bus(models.Model):
+    """
+    Buat Model Bus dengan technical name res.bus memiliki rincian field sebagai berikut: 
+        a. name -> Type: Char, String: Name 
+        b. code -> Type: Char, String: Code 
+        c. capacity -> Type: Integer, String: Capacity 
+        d. image ->Type: Binary, String: Image
+    """
+    
+    _name = 'res.bus'
+    _description = 'Bus'
+    
+    name = fields.Char(string='Name')
+    code = fields.Char(string='Code')
+    capacity = fields.Integer(string='Capacity')
+    image = fields.Image(string='Image')
+    
---
+++ models/passenger.py
@@ -0,0 +1,20 @@
+-*- coding: utf-8 -*-
+
+from odoo import models, fields 
+
+class Passenger(models.Model):
+    """
+    Buat Model Passenger dengan technical name res.passenger memiliki rincian field sebagai berikut:
+        a. name -> Type: Char, String: Name 
+        b. weight -> Type: Float, String: Weight(Kg) 
+        c. height -> Type: Float, String: Height(Cm) 
+        d. born_date -> Type: Date, String: Born Date
+    """
+    
+    _name = 'res.passenger'
+    _description = 'Passenger'
+    
+    name = fields.Char(string='Name')
+    weight = fields.Float(string='Weight(Kg)')
+    height = fields.Float(string='Height(Cm)')
+    born_date = fields.Date(string='Born Date')
